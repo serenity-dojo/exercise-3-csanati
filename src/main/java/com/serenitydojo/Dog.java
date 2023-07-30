8d8dfe79e5e65bc1276e5acab5e0ab9a9d94d42a
csanati <christina.sanati@wellsky.com>
2023-07-30 00:55:21 -0500

    add Dog class with appropriate getters and add logic to WhenCreatingObjects class

--- src/main/java/com/serenitydojo/Dog.java
+++ src/main/java/com/serenitydojo/Dog.java
@@ -4,6 +4,9 @@ public class Dog {
     private String name;
     private String favoriteToy;
     private int age;
+    private boolean isFed;
+
+    static String DOG_NOISE = "Woof";
 
     public Dog(String name, String favoriteToy, int age) {
         this.name = name;
@@ -18,5 +21,11 @@ public class Dog {
     }
     public int getAge(){
         return age;
+    }
+    public void makeNoise(){
+        System.out.println(DOG_NOISE);
+    }
+    public void feed(){
+        this.isFed = true;
     }
 }
